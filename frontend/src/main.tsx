391aa1b1a960b790da00f4fd768548d64ef54d0f
dlpduy <dinhlephucduy@gmail.com>
2024-11-13 23:18:54 +0700

    #5 - dlpduy create pages login, register, resetpassword

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import './styles/global.css'
+import LoginPage from './pages/login'
+import Header from './components/layout/header'
+import RegisterPage from './pages/register'
+import ForgotPassWordPage from './pages/forgotpassword'
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <div>Home</div>,
+    path: "/login",
+    element:
+      <>
+        <Header
+          isLogin={false}
+        />
+        <LoginPage />
+      </>
+  },
+  {
+    path: "/register",
+    element: <>
+      <Header
+        isLogin={false}
+      />
+      <RegisterPage />
+    </>
+
+  },
+  {
+    path: "/forgot-password",
+    element: <>
+      <Header
+        isLogin={false}
+      />
+      <ForgotPassWordPage />
+    </>
   },
 ])
 
