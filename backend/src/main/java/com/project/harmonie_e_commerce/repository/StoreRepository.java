1f6326a0a72875f95f5ba45a0f5bd36051bfaa6a
Phuc Tran Hong <tranhongphucfb@gmail.com>
2024-12-04 00:05:37 +0700

    donothing

---
+++ backend/src/main/java/com/project/harmonie_e_commerce/repository/StoreRepository.java
@@ -0,0 +1,8 @@
+package com.project.harmonie_e_commerce.repository;
+
+import com.project.harmonie_e_commerce.model.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Integer> {
+
+}
