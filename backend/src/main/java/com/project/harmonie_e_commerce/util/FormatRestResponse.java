02ae41be1c8872b7e9f866beb2c09d8759df46af
leminhthe04 <the.leminh04@hcmut.edu.vn>
2024-12-09 11:17:06 +0700

    fixed wrong error respone fotmet, add id in entity respone, remove try...catch in OrderController

--- backend/src/main/java/com/project/harmonie_e_commerce/util/FormatRestResponse.java
+++ backend/src/main/java/com/project/harmonie_e_commerce/util/FormatRestResponse.java
@@ -1,6 +1,5 @@
 package com.project.harmonie_e_commerce.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.harmonie_e_commerce.response.RestResponse;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
@@ -9,7 +8,6 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-
 
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -35,48 +33,19 @@ public class FormatRestResponse implements ResponseBodyAdvice<Object> {
         int status = servletResponse.getStatus();
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(status);
+        if (body instanceof String)
+            return body;
 
+        if (status >= 400) {
+            return body;
+        } else {
+            res.setData(body);
+            ApiMessage message = returnType.getMethodAnnotation(ApiMessage.class);
+            res.setMessage(message != null ? message.value() : "CALL API SUCCESS");
 
-        // if (body instanceof String)
-        //     return body;
-
-        // if (status >= 400) {
-        //     return body;
-        // } else {
-        //     res.setData(body);
-        //     ApiMessage message = returnType.getMethodAnnotation(ApiMessage.class);
-        //     res.setMessage(message != null ? message.value() : "CALL API SUCCESS");
-
-        // }
-
-        // return res;
-        
-
-
-    // If the body is a string (string from error message exception or inserted noitification,...)
-    // needs to handle it differently
-    if (body instanceof String) {
-        res.setMessage(body);
-        res.setError(status >= 400 ? "BAD REQUEST" : null);
-        res.setData(null); 
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            // convert object to JSON format
-            String jsonResponse = objectMapper.writeValueAsString(res);
-            // set headers to json to let spring know that response is json
-            response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-            return jsonResponse;
-        } catch (Exception e) {
-            throw new RuntimeException("Error serializing response", e);
         }
-    }
 
-    // body is other objects
-    res.setData(body);
-    ApiMessage message = returnType.getMethodAnnotation(ApiMessage.class);
-    res.setMessage(message != null ? message.value() : "CALL API SUCCESS");
-
-    return res;
+        return res;
     }
 
 }
